d4693665c367589d1691e8bc3758816a35f275d9
Sebastien Arod <sebastien.arod@semarchy.com>
2020-03-09 09:29:35 +0100

    tycho - junit5 - mockito

---
+++ tycho-bugs/junit5/bundle1.tests/src/bundle1/TestUsingMockito.java
@@ -0,0 +1,17 @@
+package bundle1;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(org.mockito.junit.jupiter.MockitoExtension.class)
+public class TestUsingMockito {
+
+    @org.mockito.Mock
+    private Object myMock;
+
+
+    @Test
+    void myTest() {
+
+    }
+}
